36f015ab230d8a706e64a6268df80edfbbc81e35
madhav <eragon.cube@outlook.com>
2024-01-09 20:53:11 +0530

    Fix signOut callbackUrl in page.tsx

--- app/signout/page.tsx
+++ app/signout/page.tsx
@@ -3,7 +3,7 @@
 import { signOut } from 'next-auth/react';
 
 export default function SignIn() {
-  signOut('google', {
-    callbackUrl: '/profile'
+  signOut({
+    callbackUrl: '/profile',
   });
 }
