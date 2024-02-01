7f48f1295ca47a7ab4c649d6eb5e05ff076713e3
madhav <eragon.cube@outlook.com>
2024-02-01 13:14:53 +0530

    Refactor code for improved performance

--- app/api/razorpay/route.ts
+++ app/api/razorpay/route.ts
@@ -1,6 +1,22 @@
 import Razorpay from 'razorpay';
+import { config } from 'dotenv';
+import { object, string } from 'zod';
+config({ path: '.env.local' });
+
+const schema = object({
+  RAZORPAY_API_KEY: string({
+    required_error: 'Razorpay API key is required',
+  }).min(1),
+  RAZORPAY_API_SECRET: string({
+    required_error: 'Razorpay API secret is required',
+  }).min(1),
+});
+
+const env = schema.parse(process.env);
+
+const { RAZORPAY_API_KEY, RAZORPAY_API_SECRET } = env;
 
 const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_API_KEY,
-  key_secret: process.env.RAZORPAY_API_SECRET,
+  key_id: RAZORPAY_API_KEY,
+  key_secret: RAZORPAY_API_SECRET,
 });
