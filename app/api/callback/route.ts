7f4be72aa951ccff6cf67e18f63aa2e00336dc24
madhav <eragon.cube@outlook.com>
2024-02-03 18:33:53 +0530

    Refactor Razorpay webhook callback route

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -3,19 +3,22 @@ import { createHmac } from 'crypto';
 import { RAZORPAY_API_SECRET } from '@/config';
 // import handleSubmit from '@/app/_actions/handleSubmit';
 
-export async function POST(req: Request, res: Response) {
-  const { paymentId, orderId, signature } = await req.json();
-  // const searchParams = new URL(req.url).searchParams;
-  // const orderId = searchParams.get('orderId') as string;
-  const generated = hash(orderId + '|' + paymentId);
-  // console.log(generated);
-  // console.log(arr);
-  if (generated !== signature) {
+export async function POST(req: Request, _: Response) {
+  try {
+    const { paymentId, orderId, signature } = await req.json();
+    const generated = hash(orderId + '|' + paymentId);
+
+    if (generated !== signature) {
+      throw new Error('Signature validation failed');
+    }
+
+    return NextResponse.redirect(new URL('/profile', req.url));
+  } catch (error) {
+    console.error('Error processing Razorpay webhook:', error);
     return NextResponse.error();
   }
-
-  return NextResponse.redirect(new URL('/profile', req.url));
 }
+
 
 function hash(string: string) {
   return createHmac('sha256', RAZORPAY_API_SECRET).update(string).digest('hex');
