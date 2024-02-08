abadc0fcc5b5b948657389568a1ec690eca760f7
madhav <eragon.cube@outlook.com>
2024-02-08 21:17:36 +0530

    Add icons to FormWrapper component

--- components/FormWrapper.tsx
+++ components/FormWrapper.tsx
@@ -31,6 +31,86 @@ export default function FormWrapper({
       <main className='flex flex-col items-center justify-center w-full text-center h-full'>
         {children}
       </main>
+      <div className='flex flex-row-reverse mt-4 gap-4 md:scale-100 scale-75 md:w-full w-[116.67%]'>
+        <svg
+          width={24}
+          height={24}
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          xmlnsXlink='http://www.w3.org/1999/xlink'
+        >
+          <rect width={24} height={24} fill='url(#pattern0)' />
+          <defs>
+            <pattern
+              id='pattern0'
+              patternContentUnits='objectBoundingBox'
+              width={1}
+              height={1}
+            >
+              <use xlinkHref='#image0_36_275' transform='scale(0.1)' />
+            </pattern>
+            <image
+              id='image0_36_275'
+              width={10}
+              height={10}
+              xlinkHref='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAABYlAAAWJQFJUiTwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADySURBVHgBVVAxUsMwENw9PJYpmFDwAYaWIj8Izg9oKBmGjzh+SUxFSQdlSApqPsAbYIbaWk6ymCS6OUl3e7d7ElHWxysWTY1VCJw3AeehxntT6eniGkPCLW2bF3QUNwQW7jMIIHHDyta/X+hSDd+e8dAErpsgORudTe75nGLJoGVlxD2gzOFscqNwuDx/gq6KYptCTU63KUDpU0pwbt7+wwJl2O9eov+IltQ088foMzMVSEXuWN52VldYZcjyfGnaSUD7To0YbHmHrRP0mYpKstg3CjGyP70ch/yP7W10Vnv06XZ5SuB7HLGNUHt2FfuU+AOqPVhYbfAI2AAAAABJRU5ErkJggg=='
+            />
+          </defs>
+        </svg>
+        <svg
+          width={24}
+          height={24}
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          xmlnsXlink='http://www.w3.org/1999/xlink'
+        >
+          <rect width={24} height={24} fill='url(#pattern0)' />
+          <defs>
+            <pattern
+              id='pattern0'
+              patternContentUnits='objectBoundingBox'
+              width={1}
+              height={1}
+            >
+              <use xlinkHref='#image0_36_275' transform='scale(0.1)' />
+            </pattern>
+            <image
+              id='image0_36_275'
+              width={10}
+              height={10}
+              xlinkHref='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAABYlAAAWJQFJUiTwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADySURBVHgBVVAxUsMwENw9PJYpmFDwAYaWIj8Izg9oKBmGjzh+SUxFSQdlSApqPsAbYIbaWk6ymCS6OUl3e7d7ElHWxysWTY1VCJw3AeehxntT6eniGkPCLW2bF3QUNwQW7jMIIHHDyta/X+hSDd+e8dAErpsgORudTe75nGLJoGVlxD2gzOFscqNwuDx/gq6KYptCTU63KUDpU0pwbt7+wwJl2O9eov+IltQ088foMzMVSEXuWN52VldYZcjyfGnaSUD7To0YbHmHrRP0mYpKstg3CjGyP70ch/yP7W10Vnv06XZ5SuB7HLGNUHt2FfuU+AOqPVhYbfAI2AAAAABJRU5ErkJggg=='
+            />
+          </defs>
+        </svg>
+        <svg
+          width={24}
+          height={24}
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          xmlnsXlink='http://www.w3.org/1999/xlink'
+        >
+          <rect width={24} height={24} fill='url(#pattern0)' />
+          <defs>
+            <pattern
+              id='pattern0'
+              patternContentUnits='objectBoundingBox'
+              width={1}
+              height={1}
+            >
+              <use xlinkHref='#image0_36_275' transform='scale(0.1)' />
+            </pattern>
+            <image
+              id='image0_36_275'
+              width={10}
+              height={10}
+              xlinkHref='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAABYlAAAWJQFJUiTwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADySURBVHgBVVAxUsMwENw9PJYpmFDwAYaWIj8Izg9oKBmGjzh+SUxFSQdlSApqPsAbYIbaWk6ymCS6OUl3e7d7ElHWxysWTY1VCJw3AeehxntT6eniGkPCLW2bF3QUNwQW7jMIIHHDyta/X+hSDd+e8dAErpsgORudTe75nGLJoGVlxD2gzOFscqNwuDx/gq6KYptCTU63KUDpU0pwbt7+wwJl2O9eov+IltQ088foMzMVSEXuWN52VldYZcjyfGnaSUD7To0YbHmHrRP0mYpKstg3CjGyP70ch/yP7W10Vnv06XZ5SuB7HLGNUHt2FfuU+AOqPVhYbfAI2AAAAABJRU5ErkJggg=='
+            />
+          </defs>
+        </svg>
+      </div>
     </div>
   );
 }
