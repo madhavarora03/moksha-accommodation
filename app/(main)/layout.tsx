94886ce144baf263353795d563f57945fbab14da
madhav <eragon.cube@outlook.com>
2024-02-09 10:54:23 +0530

    Delete unnecessary files and update Header component width

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,10 @@
+import Footer from '@/components/Footer';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {children}
+      <Footer />
+    </div>
+  );
+}
